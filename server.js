21a5b743ce1497e153e0d4eafd4e0cf0075b5e63
unknown <mistrzuz122@gmail.com>
2019-05-13 19:11:20 +0200

    Dodawanie funkcji logowania

--- server.js
+++ server.js
@@ -21,7 +21,32 @@ const db = knex({
   }
 });
 
-app.get('/', (req,res) => res.send(db.users));
+app.get('/', (req,res) =>  db.select('*').from('users').then(data => res.send(data)));
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('Brak emaila/hasła');
+  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('błąd podczas pobierania informacji z bazy danych'))
+      } else {
+        res.status(400).json('Błędny login/hasło')
+      }
+    })
+    .catch(err => res.status(400).json('Błędny login/hasło'))
+  })
+  app.post('/rejestracja', (req, res) => {
+    const { email, name, password } = req.body;
+  })
 
 app.listen(port, ()=> {
   console.log(`app is running on port ${port}`);
